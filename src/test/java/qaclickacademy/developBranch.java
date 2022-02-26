fdf7e5026e3e8362cbb5020ce707bdf77796167f
angu <anguvelgautham1997@gmail.com>
2022-02-27 00:32:56 +0530

    Workspace2 commit

--- src/test/java/qaclickacademy/developBranch.java
+++ src/test/java/qaclickacademy/developBranch.java
@@ -5,5 +5,11 @@ public class developBranch {
 	public static void main() {
 		System.out.println("new branch created");
 	}
+	
+	public void func() {
+		System.out.println("new branch function new");
+	}
+	
+	
 
 }
